6f5464ff9383bb1b8476e60a18a058937af5e268
Joseph-Chege <mjchege23@gmail.com>
2024-06-26 20:34:22 +0300

    Sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    '''prints fibonacci sequence of length'''
+    if length <= 0:
+        print([])
+        return
+    # Initialize the first two Fibonacci numbers
+    fib_sequence = [0, 1]
+    
+    # Generate the Fibonacci sequence up to the desired length
+    while len(fib_sequence) < length:
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+
+    # Adjust the length of the sequence if n is 1
+    if length == 1:
+        fib_sequence = [0]
+    
+    print(fib_sequence)
+       
+
+
+
     pass
+
+#Fibonnaci sequence is the two previous elements added to give the next element
+#0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 570288
